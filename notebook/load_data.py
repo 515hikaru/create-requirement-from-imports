6a2a6c36a2b6e9055afc8693d1b1ef44975054ed
515hikaru <12kojima.takahiro@gmail.com>
2020-01-25 17:29:18 +0900

    add files

---
+++ notebook/load_data.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+df = pd.read_csv('data.csv')
+
+
+# In[3]:
+
+
+...
+
+
+# In[4]:
+
+
+def foo():
+    import matplotlib
+
+
+# In[5]:
+
+
+import torch.nn as nn
+
+
+# In[ ]:
+
+
+
+
